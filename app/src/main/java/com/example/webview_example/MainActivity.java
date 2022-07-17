ef0bea2d51c4043b451c7fea808033169527aac2
Ashwaganha-cader <max.cacz@yandex.ru>
2022-07-17 22:06:15 +0500

    done

--- app/src/main/java/com/example/webview_example/MainActivity.java
+++ app/src/main/java/com/example/webview_example/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.webview_example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView browser=findViewById(R.id.webBrowser);
+        browser.loadUrl("http://shift74.ru");
     }
 }
